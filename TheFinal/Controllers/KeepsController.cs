a68a805763f05a32236a3489b2e2f7dfbd2fa6ea
Jonathon McNamara <jonmcnamara1234@hotmail.com>
2022-09-23 11:36:21 -0600

    new repos and controllers for keeps

---
+++ TheFinal/Controllers/KeepsController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+using TheFinal.Services;
+
+namespace TheFinal.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class KeepsController : ControllerBase
+    {
+        private readonly KeepsService _keepsService;
+
+        public KeepsController(KeepsService keepsService)
+        {
+            _keepsService = keepsService;
+        }
+    }
+}
---
+++ TheFinal/Services/KeepsService.cs
@@ -0,0 +1,14 @@
+using TheFinal.Repositories;
+
+namespace TheFinal.Services
+{
+    public class KeepsService
+    {
+        private readonly KeepsRepository _keepsRepo;
+
+        public KeepsService(KeepsRepository keepsRepo)
+        {
+            _keepsRepo = keepsRepo;
+        }
+    }
+}
